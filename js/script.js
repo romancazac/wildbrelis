1eafaac110671be8533700a90e147782e1820464
Roman Cazac <romancazac23@icloud.com>
2022-03-04 19:43:35 +0200

    server requests

--- js/script.js
+++ js/script.js
@@ -3,18 +3,39 @@ const cart = function(){
     const cartBtn = document.querySelector('.button-cart');
     const modalCart = document.querySelector('#modal-cart');
     const closeBtn = modalCart.querySelector('.modal-close');
-    cartBtn.addEventListener('click', function(){
+     cartBtn.addEventListener('click', function(){
         modalCart.style.display='flex';
+        modalCart.classList.add('open');
     });
     closeBtn.addEventListener('click', function(){
         modalCart.style.display='';
+        modalCart.classList.remove('open');
     });
 }
 cart();
 
 const getGoods = function (){
+    const getLinks = document.querySelectorAll('.navigation-link');
     const getData = () =>{
-        fetch('')
+        fetch('https://project1-754e0-default-rtdb.firebaseio.com/db.json')
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data)
+        }) 
     }
+    
+    getLinks.forEach((item)=>{
+        item.addEventListener("click", (e) => {
+            e.preventDefault();
+            getData();
+            console.log(e.target)
+            
+        })
+    })
+    localStorage.setItem('goods', JSON.stringify('getData'))
+    const goods = JSON.parse(localStorage.getItem('goods'))
+    console.log(goods)
+    // localStorage.removeItem('goods')
+
 };
 getGoods();
